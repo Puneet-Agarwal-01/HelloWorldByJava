2501f991a58df1c493b089af55d7baa166848bb6
Puneet-Agarwal-01 <63462560+Puneet-Agarwal-01@users.noreply.github.com>
2020-07-29 19:10:12 +0530

    Create HelloWorld.java
    OUTPUT -->
    
    Hello World!

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+package helloWorld;                                         //Creating package
+public class HelloWorld{                                     //Creating class
+  public static void main(String[] args){                       //Main Method
+    System.out.println("Hello World!");                       //Printing statement
+  }
+}
